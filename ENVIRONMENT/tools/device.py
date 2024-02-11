a3bc2a04f16340ef153601ab9ced2731c20eebba
superintgr <155528797+superintgr@users.noreply.github.com>
2024-02-11 08:19:03 -0500

    Delete ENVIRONMENT/tools directory

--- ENVIRONMENT/tools/device.py
+++
@@ -1,8 +0,0 @@
-from pynput import mouse, keyboard
-from pynput.keyboard import Key, Controller
-
-keyboard =  Controller()
-press = lambda key: keyboard.press(key)
-release = lambda key: keyboard.release(key)
-shortcut = lambda string: keyboard.type(string)
-
